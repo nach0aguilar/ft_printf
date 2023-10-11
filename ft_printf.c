c5e8a9ab2522341fbb98af7324d548e79929a1df
Ignacio Aguilar Fernandez <igaguila@c3r4s1.42malaga.com>
2023-10-11 18:30:09 +0200

    control de char, string, int

--- ft_printf.c
+++ ft_printf.c
@@ -6,44 +6,47 @@
 /*   By: igaguila <igaguila@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/03 11:27:56 by igaguila          #+#    #+#             */
-/*   Updated: 2023/10/10 19:24:05 by igaguila         ###   ########.fr       */
+/*   Updated: 2023/10/11 18:28:19 by igaguila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-static void ft_putchar_fd(char c, int fd)
+static void	ft_putchar_fd(char c, int fd)
 {
 	write(fd, &c, 1);
 }
 
-int ft_printf(char const *s, ...)
+int	ft_printf(char const *s, ...)
 {
-    va_list arguments;
-    va_start(arguments, s);
-    int i;
-    
-    i = 0;
-    while (s[i])
-    {
-        if (s[i] == '%')
-        {
-            i++;
-            ft_varprint(s[i]);
-        }
-        else
-            ft_putchar_fd(s[i], 1);
-        i++;
-    }
-    
-    va_end(arguments);
-    return (0);
+	va_list	arguments;
+	int		i;
+
+	va_start(arguments, s);
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == '%')
+		{
+			i++;
+			if (s[i] == '%')
+        		ft_putchar_fd('%', 1);
+			else
+				ft_varprint(s[i], va_arg(arguments, void *));
+		}
+		else
+			ft_putchar_fd(s[i], 1);
+		i++;
+	}
+	va_end(arguments);
+	return (0);
 }
 
-int main()
+int	main(void)
 {
-    char chr = 'A';
-    char *str = "Hola";
-    int num = 345;
-    ft_printf("hola esto es una prueba %c %% %s %d\n", chr, str, num);
+	char chr = 'A';
+	char *str = "Pepe";
+	int num = 345;
+	ft_printf("hola %c que %d tal %s\n", chr, num, str);
+	printf("hola %c que %d tal %s\n", chr, num, str);
 }
--- ft_varprint.c
+++ ft_varprint.c
@@ -6,7 +6,7 @@
 /*   By: igaguila <igaguila@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/04 14:57:34 by igaguila          #+#    #+#             */
-/*   Updated: 2023/10/11 11:17:41 by igaguila         ###   ########.fr       */
+/*   Updated: 2023/10/11 18:01:43 by igaguila         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,39 +29,37 @@ static void	ft_putstr_fd(char *s, int fd)
 	}
 }
 
-// static void	ft_putnbr_fd(int n, int fd)
-// {
-// 	if (n == -2147483648)
-// 		ft_putstr_fd("-2147483648", fd);
-// 	else
-// 	{
-// 		if (n < 0)
-// 		{
-// 			ft_putchar_fd('-', fd);
-// 			n *= -1;
-// 		}
-// 		if (n >= 10)
-// 			ft_putnbr_fd(n / 10, fd);
-// 		ft_putchar_fd((n % 10) + '0', fd);
-// 	}
-// }
+static void	ft_putnbr_fd(int n, int fd)
+{
+	if (n == -2147483648)
+		ft_putstr_fd("-2147483648", fd);
+	else
+	{
+		if (n < 0)
+		{
+			ft_putchar_fd('-', fd);
+			n *= -1;
+		}
+		if (n >= 10)
+			ft_putnbr_fd(n / 10, fd);
+		ft_putchar_fd((n % 10) + '0', fd);
+	}
+}
 
-void    ft_varprint(char c)
+void    ft_varprint(char c, void *ptr)
 {
     if (c == 'c')
-        ft_putstr_fd("chr", 1);
+        ft_putchar_fd((char)ptr, 1);
     else if (c == 's')
-        ft_putstr_fd("string", 1);
+        ft_putstr_fd((char *)ptr, 1);
     // else if (c == 'p')
     //     c;
     else if (c == 'd' || c == 'i')
-        ft_putstr_fd("int", 1);
+        ft_putnbr_fd((int)ptr, 1);
     // else if (c == 'u')
     //     c;
     // else if (c == 'x')
     //     c;
     // else if (c == 'X')
-    //     c;
-    else if (c == '%')
-        ft_putchar_fd('%', 1);        
+    //     c;        
 }
